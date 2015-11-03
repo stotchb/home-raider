4fe29900cf20026514508410e74bd3c6ea95122f
YingTsung Lee <ytli@intowow.com>
2015-11-04 01:39:25 +0800

    Add tsd.json and type definition files

---
+++ home-raider/Crawlers/app.d.ts
@@ -0,0 +1,4 @@
+﻿///<reference path='typings/node/node.d.ts'/>
+///<reference path='typings/cheerio/cheerio.d.ts'/>
+///<reference path='typings/lodash/lodash.d.ts'/>
+///<reference path='typings/bluebird/bluebird.d.ts'/>
--- home-raider/Crawlers/app.ts
+++ home-raider/Crawlers/app.ts
@@ -1,1 +1,10 @@
-﻿console.log('Hello world');
+﻿///<reference path='app.d.ts'/>
+
+import cheerio = require('cheerio');
+import _ = require('lodash');
+
+var $: any = cheerio.load('<span></span>');
+console.log($.html());
+
+var arr: number[] = [1, 2, 3, 4, 5];
+console.log(_.map(arr, (x) => x * 2));
