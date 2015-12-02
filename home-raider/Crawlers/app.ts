6feec443f1c5a5e2f30272c8d4dcaac1ff6cb771
YingTsung Lee <ytli@intowow.com>
2015-12-03 01:56:41 +0800

    Test cheerio/request for 591 parser

--- home-raider/Crawlers/app.ts
+++ home-raider/Crawlers/app.ts
@@ -1,10 +1,49 @@
 ﻿///<reference path='app.d.ts'/>
 
-import cheerio = require('cheerio');
+import cheerio  = require('cheerio');
 import _ = require('lodash');
+import sprintf = require('sprintf');
+import request = require('request');
+import Promise = require('bluebird');
 
-var $: any = cheerio.load('<span></span>');
-console.log($.html());
 
-var arr: number[] = [1, 2, 3, 4, 5];
-console.log(_.map(arr, (x) => x * 2));
+getPage('http://www.591.com.tw/rent-detail-3784790.html')
+    .then(function (data) {
+        retrieve591(data);
+    })
+    .catch(function (err) {
+        console.error(err);
+    });
+
+/**
+ * 
+ * @param url target url
+ */
+function getPage(url) {
+    return new Promise(function (resolve, reject) {
+        return request(url, function (err, resp, body) {
+            if (err) {
+                return reject(err);
+            }
+
+            return resolve(body);            
+        })
+    });
+}
+
+function retrieve591(data) {
+    var $ = cheerio.load(data);
+    console.log(getTitle());
+    console.log(getDetail());
+
+    function getTitle() {
+        return $('#infoTitle > h1').text().trim()
+    }
+
+    function getDetail() {
+        var elem = $('#detailInfo > ul > li');
+        elem.each(function (index, item) {
+            console.log('[' + index + '] : ' + $(this).text().trim());
+        });        
+    }
+}
